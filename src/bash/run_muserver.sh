f5bb371cd572033a905c717963a3fa6acf5fcdb4
Mario h.c.t <mario.hct@gmail.com>
2013-08-20 22:49:45 +0200

    run_muserver saves its pid on /home/pi/pid now

--- src/bash/run_muserver.sh
+++ src/bash/run_muserver.sh
@@ -2,7 +2,10 @@
 /usr/bin/xset -dpms
 /usr/bin/xset s noblank
 
+BASE_FOLDER=/home/pi
 
-/home/pi/jdk1.8.0/bin/java -cp /home/pi/mu-server/bin/:/home/pi/mu-server/bin/mu-server.jar com.muframe.server.MuServer >> /home/pi/mu-server/logs.txt
+/home/pi/jdk1.8.0/bin/java -cp $BASE_FOLDER/mu-server/bin/:$BASE_FOLDER/mu-server/bin/mu-server.jar com.muframe.server.MuServer >> $BASE_FOLDER/mu-server/logs.txt &
+echo $! > $BASE_FOLDER/pid
+
 #/usr/bin/java -cp /home/pi/muserver.jar com.muframe.server.MuServer
 
