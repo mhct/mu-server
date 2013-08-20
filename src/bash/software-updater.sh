55cb2bffedf686f1403e4d67acd7848e89eead7f
Mario h.c.t <mario.hct@gmail.com>
2013-08-20 10:25:25 +0200

    added SHAsum checks, versioning and upgrading of the software.

--- src/bash/software-updater.sh
+++ src/bash/software-updater.sh
@@ -7,7 +7,8 @@
 #
 
 UPDATE_URL=http://www.missingyouframe.com/mu-updates/update.txt
-
+BASE_FOLDER=/Users/danirigolin/Documents/projetos/mu-server/src/bash/temp
+SERVER_PID=$(cat $BASE_FOLDER/pid)
 
 function fetch_update_data {
     local __update_url=$1
@@ -26,35 +27,69 @@ function fetch_update_data {
 }
 
 
-function download_update {
+function download_apply_update {
     update_url=$1
     update_sha=$2
 
     echo "Downloading updates"
     $(curl $update_url -o /tmp/mu-server-update.tar.gz)
-    # do the checksum
-    #$(shasum /tmp/mu-server-update.tar.gz
-    echo "Applying updates"
-    $(tar zxvf /tmp/mu-server-update.tar.gz -C ./)
+   
+    # 
+    # compares downloaded file SHA with the one described at the update URL
+    #
+    if [ "$update_sha" != "$(shasum /tmp/mu-server-update.tar.gz|awk '{print $1}')" ]
+    then
+            echo "Problem with the downloaded file. SHASUM is different."
+    else
+            echo "Applying updates"
+            cd 
+            $(tar zxvf /tmp/mu-server-update.tar.gz -C $BASE_FOLDER/)
+
+            #
+            # backsup previous simlink and links to new version
+            #
+            echo "Symlinking new version"
+            version_name=$(tar tvf /tmp/mu-server-update.tar.gz |awk -F/ '{if (NF<3) print}'|awk '{print $(NF-0)}')
+            rm $BASE_FOLDER/mu-server_previous
+            mv $BASE_FOLDER/mu-server $BASE_FOLDER/mu-server_previous
+            ln -s $BASE_FOLDER/$version_name $BASE_FOLDER/mu-server
+
+            touch_version_file $update_sha
+    fi
 }
 
 function touch_version_file {
-    echo "$1" > ./current_version.txt
+    echo "Updating current_version.txt to: " $1
+    echo "$1" > $BASE_FOLDER/current_version.txt
+}
+
+#
+# TODO improve error checking, for loading the new software... perhaps it could invoke some software
+# functions to guarantee the new version is loaded and working
+#
+function restart_mu_server {
+    echo "Restarting current software: " $SERVER_PID
+    $(kill -s 9 $SERVER_PID)
 }
 
 update_url=""
 update_sha=""
 
+cd $BASE_FOLDER
+pwd
 fetch_update_data update_url update_sha
-touch_version_file $update_sha
+#touch_version_file $update_sha
 
-if [ "$update_sha" != $(cat ./current_version.txt) ]
+#echo $update_sha
+#echo $(cat ./current_version.txt)
+
+if [ "$update_sha" != "$(cat ./current_version.txt)" ]
 then
-    download_update $update_url $update_sha
+    download_apply_update $update_url $update_sha
+    #touch_version_file $update_sha
+    restart_mu_server
+    pwd
 else
     echo "Already latest version of the software"    
 fi
-
-
-
 
