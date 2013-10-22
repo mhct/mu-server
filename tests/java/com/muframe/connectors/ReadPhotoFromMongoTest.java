beab726de70581f5a2b250e64d4b116dfc1e2ef2
Mario h.c.t <mario.hct@gmail.com>
2013-10-22 23:46:26 +0200

    commented out old code

--- tests/java/com/muframe/connectors/ReadPhotoFromMongoTest.java
+++ tests/java/com/muframe/connectors/ReadPhotoFromMongoTest.java
@@ -1,37 +1,37 @@
-package com.muframe.connectors;
-
-import java.io.File;
-import java.io.IOException;
-
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import com.mongodb.gridfs.GridFS;
-import com.mongodb.gridfs.GridFSDBFile;
-
-public class ReadPhotoFromMongoTest {
-
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
-		MongoClient client = new MongoClient("127.0.0.1", 27017);
-		DB db = client.getDB("muphotos");
-//		BasicDBObject photo = (BasicDBObject) db.getCollection("photos").findOne();
-//		BSONObject photo = (BSONObject) db.getCollection("photos").findOne();
-		GridFS gridfs = new GridFS(db);
-		GridFSDBFile file = gridfs.findOne("876c2bd8-b924-4307-9c0f-57f22dc29315");
-		
-		
-		//		DBObject oo = (DBObject) db.getCollection("photos").findOne();
-		
-//		Binary obj = (Binary) photo.get("photoID");
-		
-//		Object obj = photo.get("photoID");
-		
-//		System.out.println(obj.toString());
-		File f = new File("/tmp/876c2bd8-b924-4307-9c0f-57f22dc29315.jpg");
-		file.writeTo(f);
-	}
-
-}
+//package com.muframe.connectors;
+//
+//import java.io.File;
+//import java.io.IOException;
+//
+//import com.mongodb.DB;
+//import com.mongodb.MongoClient;
+//import com.mongodb.gridfs.GridFS;
+//import com.mongodb.gridfs.GridFSDBFile;
+//
+//public class ReadPhotoFromMongoTest {
+//
+//	/**
+//	 * @param args
+//	 * @throws IOException 
+//	 */
+//	public static void main(String[] args) throws IOException {
+//		MongoClient client = new MongoClient("127.0.0.1", 27017);
+//		DB db = client.getDB("muphotos");
+////		BasicDBObject photo = (BasicDBObject) db.getCollection("photos").findOne();
+////		BSONObject photo = (BSONObject) db.getCollection("photos").findOne();
+//		GridFS gridfs = new GridFS(db);
+//		GridFSDBFile file = gridfs.findOne("876c2bd8-b924-4307-9c0f-57f22dc29315");
+//		
+//		
+//		//		DBObject oo = (DBObject) db.getCollection("photos").findOne();
+//		
+////		Binary obj = (Binary) photo.get("photoID");
+//		
+////		Object obj = photo.get("photoID");
+//		
+////		System.out.println(obj.toString());
+//		File f = new File("/tmp/876c2bd8-b924-4307-9c0f-57f22dc29315.jpg");
+//		file.writeTo(f);
+//	}
+//
+//}
